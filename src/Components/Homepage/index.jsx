f87aaed31b62cee54a1f5052deba1f28217e6d2e
Setone01 <86173717+Setone01@users.noreply.github.com>
2023-02-07 15:17:18 +0100

    Update index.jsx

--- src/Components/Homepage/index.jsx
+++ src/Components/Homepage/index.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Courses from "./HomeComponent/COURSES/Courses";
 import Features from "./HomeComponent/FEATURES/Features";
-import Hero from "./HomeComponent/HERO/Hero";
+// import Hero from "./HomeComponent/HERO/Hero";
 import GettingStarted from "./HomeComponent/GettingStarted/index";
 
 const index = () => {
   return (
     <>
-      <Hero />
+//       <Hero />
       <Courses />
       <Features />
       <GettingStarted />
